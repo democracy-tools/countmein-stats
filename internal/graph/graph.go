31173c4efe38f59958519260ab387c48c3f2c7ad
Effi <effoeffi@users.noreply.github.com>
2023-03-16 01:28:46 +0200

    directed graph - calc node score (#2)

---
+++ internal/graph/graph.go
@@ -0,0 +1,137 @@
+package graph
+
+// Trusted - verified device
+// Flag - device seen by trusted
+// Pawn - device that is not trusted and not seen by trusted
+const (
+	TrustedScore = -1000
+	FlagScore    = -2000
+)
+
+type Directed struct {
+	trusted   map[string]*Vertex
+	untrusted map[string]*Vertex
+}
+
+type Vertex struct {
+	id    string
+	Score int
+	edges []*Edge
+}
+
+type Edge struct {
+	from *Vertex
+	to   *Vertex
+}
+
+func NewDirected() *Directed {
+
+	return &Directed{
+		trusted:   make(map[string]*Vertex),
+		untrusted: make(map[string]*Vertex),
+	}
+}
+
+func (d *Directed) Get(id string) *Vertex {
+
+	return d.untrusted[id]
+}
+
+func (d *Directed) AddEdge(fromVertexId string, fromTrusted bool, toVertexId string, toTrusted bool) {
+
+	var from, to *Vertex
+	var ok bool
+
+	// from
+	if fromTrusted {
+		from, ok = d.trusted[fromVertexId]
+		if !ok {
+			from = &Vertex{id: fromVertexId}
+			d.trusted[fromVertexId] = from
+		}
+	} else {
+		from, ok = d.untrusted[fromVertexId]
+		if !ok {
+			from = &Vertex{id: fromVertexId}
+			d.untrusted[fromVertexId] = from
+		}
+	}
+
+	// to
+	if toTrusted {
+		to, ok = d.trusted[toVertexId]
+		if !ok {
+			to = &Vertex{id: toVertexId}
+			d.trusted[toVertexId] = to
+		}
+	} else {
+		to, ok = d.untrusted[toVertexId]
+		if !ok {
+			to = &Vertex{id: toVertexId}
+			d.untrusted[toVertexId] = to
+		}
+	}
+
+	from.edges = append(from.edges, &Edge{from: from, to: to})
+}
+
+func (d *Directed) Score() {
+
+	flags := d.scoreFlags()
+	for _, v := range d.untrusted {
+		if isPawn(v) {
+			v.Score = scorePawn(v, flags)
+		}
+	}
+}
+
+func (d *Directed) scoreFlags() map[string]*Vertex {
+
+	flags := make(map[string]*Vertex)
+	for _, v := range d.trusted {
+		v.Score = TrustedScore
+		for _, currEdge := range v.edges {
+			currFlagId := currEdge.to.id
+			if _, ok := flags[currFlagId]; !ok {
+				if _, ok = d.untrusted[currFlagId]; ok {
+					currEdge.to.Score = FlagScore
+					flags[currFlagId] = currEdge.to
+				}
+			}
+		}
+	}
+
+	return flags
+}
+
+// scorePawn returns the number of flags that have path to the pawn
+func scorePawn(v *Vertex, flags map[string]*Vertex) int {
+
+	score := 0
+	for _, currFlag := range flags {
+		if findPath(currFlag, v.id) {
+			score++
+		}
+	}
+
+	return score
+}
+
+func findPath(from *Vertex, to string) bool {
+
+	for _, currEdge := range from.edges {
+		if currEdge.to.id == to {
+			return true
+		}
+		if findPath(currEdge.to, to) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isPawn(v *Vertex) bool {
+
+	return v.Score != FlagScore
+}
---
+++ internal/graph/graph_test.go
@@ -0,0 +1,51 @@
+package graph
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// 1 -> 2 -> 4
+// 1 -> 2 -> 5
+// 1 -> 2 -> 7 -> 6
+// 1 -> 3 -> 8 -> 9 -> 6
+// 7 -> 9
+func TestDirected_Score(t *testing.T) {
+
+	directed := NewDirected()
+	directed.AddEdge("1", true, "2", false)
+	directed.AddEdge("2", false, "4", false)
+	directed.AddEdge("2", false, "5", false)
+	directed.AddEdge("2", false, "7", false)
+	directed.AddEdge("7", false, "6", false)
+	directed.AddEdge("1", true, "3", false)
+	directed.AddEdge("3", false, "8", false)
+	directed.AddEdge("8", false, "9", false)
+	directed.AddEdge("9", false, "6", false)
+	directed.AddEdge("7", false, "9", false)
+
+	directed.Score()
+
+	require.Equal(t, FlagScore, directed.Get("2").Score)
+	require.Equal(t, FlagScore, directed.Get("3").Score)
+	require.Equal(t, 1, directed.Get("4").Score)
+	require.Equal(t, 1, directed.Get("5").Score)
+	require.Equal(t, 2, directed.Get("6").Score)
+	require.Equal(t, 1, directed.Get("7").Score)
+	require.Equal(t, 1, directed.Get("8").Score)
+	require.Equal(t, 2, directed.Get("9").Score)
+}
+
+// 1 -> 2
+// 3 -> 2
+func TestDirected_Score_MultipleTrustedFlagEdges(t *testing.T) {
+
+	directed := NewDirected()
+	directed.AddEdge("1", true, "2", false)
+	directed.AddEdge("3", true, "2", false)
+
+	directed.Score()
+
+	require.Equal(t, FlagScore, directed.Get("2").Score)
+}
